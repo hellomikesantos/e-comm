682cd79fed20566815de9f8efde4e26bce9e8528
Michael Santos <100429563+hellomikesantos@users.noreply.github.com>
2023-05-12 19:56:21 -0500

    Add Navigation

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Nav from "@/components/Nav";
 import { useSession, signIn, signOut } from "next-auth/react"
 
 export default function Home() {
@@ -13,6 +14,7 @@ export default function Home() {
   }
   return (
     <div className="bg-blue-900 min-h-screen">
+      <Nav />
       <div>logged in as {session.user.email}</div>
     </div>
   )
